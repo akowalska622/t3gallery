a7c22671424ec9dd3e2637a13d35664fd12143a6
Ania Kowalska <a.kowalska622@gmail.com>
2024-05-02 10:31:44 +0200

    add mocked images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+const mockUrls = [
+  "https://utfs.io/f/7b66f05d-9093-47e4-8ed9-6999d2cf081c-1xckke.jpeg",
+  "https://utfs.io/f/95750e94-9bee-4cad-800c-9506fdf98915-1xd2eo.jpeg",
+  "https://utfs.io/f/032752a7-474f-4fa5-97ee-b8624cea0361-1xd3ua.jpeg",
+  "https://utfs.io/f/dec952c0-ff6d-4c15-85e9-d56094d6244c-1xd3sq.jpeg",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (gallery in progress...)
+    <main>
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt="image" />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
