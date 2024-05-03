36366fd495120b3bb6fe8a9d1aa2c6269065a2be
Ania Kowalska <a.kowalska622@gmail.com>
2024-05-03 16:03:06 +0200

    start implementing parallel routes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,8 +22,10 @@ export const metadata = {
 
 export default function RootLayout({
   children,
+  modal,
 }: {
   children: React.ReactNode;
+  modal: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
@@ -32,6 +34,8 @@ export default function RootLayout({
         <body className={`font-sans ${inter.variable} flex flex-col gap-4`}>
           <TopNav />
           {children}
+          {modal}
+          <div id="modal-root"></div>
         </body>
       </html>
     </ClerkProvider>
