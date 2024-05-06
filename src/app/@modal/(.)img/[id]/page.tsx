9a868d66e521d6f6e9cac95fdaf5cf905d4e20bf
Ania Kowalska <a.kowalska622@gmail.com>
2024-05-06 20:01:49 +0200

    fix import

--- src/app/@modal/(.)img/[id]/page.tsx
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { Modal } from "./modal";
-import { FullPageImageView } from "~/common/full-page-image-view";
+import { FullPageImageView } from "~/components/full-image-page";
 
 export default async function PhotoModal({
   params: { id: photoId },
